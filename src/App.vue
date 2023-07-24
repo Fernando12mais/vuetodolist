be97f3679a07f17f33c75eff39d00990e859747d
fernando12mais <fernandogabriel12mais@gmail.com>
2023-07-24 08:48:50 -0300

    Adds title

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,11 @@ function removeTask(task: Task) {
   localStorage.setItem("tasks", JSON.stringify(tasks.value));
 }
 
+function removeAllTask() {
+  tasks.value = [];
+  localStorage.removeItem("tasks");
+}
+
 function completeTask(index: Task["index"]) {
   const task = tasks.value[index];
   tasks.value[index].compĺeted = !task.compĺeted;
@@ -74,38 +79,49 @@ onMounted(() => {
     </section>
 
     <section class="w-full">
-      <h1>{{ tasks.length ? "Tarefas:" : "Crie uma tarefa" }}</h1>
-
+      <h1>
+        {{ tasks.length ? `Tarefas:(${tasks.length})` : "Crie uma tarefa" }}
+      </h1>
       <div
         v-if="tasks.length"
-        class="bg-slate-800/50 p-3 rounded w-full max-w-lg mx-auto animate-fadeIn"
+        class="max-w-lg mx-auto flex flex-col gap-3 bg-slate-600 p-4 rounded"
       >
-        <ol class="flex flex-col gap-4">
-          <li
-            v-for="(task, index) in tasks"
-            :key="task.id"
-            :class="`flex items-center justify-between bg-slate-400/20 p-2 rounded uppercase animate-fadeIn ${
-              task.compĺeted ? 'bg-cyan-900' : ''
-            }`"
-          >
-            <span
-              :class="
-                task.compĺeted && 'line-through text-emerald-300 font-bold'
-              "
+        <Button
+          variant="delete"
+          v-if="tasks.length > 3"
+          :onclick="removeAllTask"
+          >Deletar tudo</Button
+        >
+        <div
+          class="bg-slate-800/70 p-3 rounded w-full animate-fadeIn max-h-96 overflow-auto flex flex-col gap-4 relative"
+        >
+          <ol class="flex flex-col gap-4">
+            <li
+              v-for="(task, index) in tasks"
+              :key="task.id"
+              :class="`flex items-center justify-between bg-slate-400/20 p-2 rounded uppercase animate-fadeIn ${
+                task.compĺeted ? 'bg-cyan-900' : ''
+              }`"
             >
-              {{ task.task }}
-            </span>
-
-            <div class="flex items-center gap-4">
-              <Button variant="done" :onclick="() => completeTask(index)">{{
-                task.compĺeted ? "Desfazer" : "Feito"
-              }}</Button>
-              <Button variant="delete" :onclick="() => removeTask(task)"
-                >Deletar tarefa</Button
+              <span
+                :class="
+                  task.compĺeted && 'line-through text-emerald-300 font-bold'
+                "
               >
-            </div>
-          </li>
-        </ol>
+                {{ task.task }}
+              </span>
+
+              <div class="flex items-center gap-4">
+                <Button variant="done" :onclick="() => completeTask(index)">{{
+                  task.compĺeted ? "Desfazer" : "Feito"
+                }}</Button>
+                <Button variant="delete" :onclick="() => removeTask(task)"
+                  >Deletar</Button
+                >
+              </div>
+            </li>
+          </ol>
+        </div>
       </div>
     </section>
   </main>
